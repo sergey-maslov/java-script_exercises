c77bdef4092c4bd70936130063244106fde4f97e
sergey-maslov <shift.tfihs@gmail.com>
2022-09-22 14:17:40 +0000

    exercises 1-2

---
+++ exercises.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+function stringToArray(string){
+    return array = string.split(" ");
+  }
+
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+function DNAtoRNA(dna) {
+    const reg = /T/gi;
+    return rna = dna.replace(reg, 'U');
+  }
