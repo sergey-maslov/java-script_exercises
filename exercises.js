2b70bfa2e864135699d4f53ac6f452f913943408
sergey-maslov <97842907+sergey-maslov@users.noreply.github.com>
2022-09-23 00:40:22 +0300

    Update exercises.js
    exercise 5

--- exercises.js
+++ exercises.js
@@ -29,3 +29,19 @@ function min(arr, toReturn) {
   const minNum = Math.min(...arr);
   return toReturn === "value" ? minNum : arr.indexOf(minNum);
 }
+
+// https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+
+function warnTheSheep(queue) {
+  
+  function findWolf(animal) {
+    return animal === "wolf";
+  }
+  
+  const index = queue.findIndex(findWolf);
+  
+  if ( index === queue.length - 1 ) {
+    return "Pls go away and stop eating my sheep";
+  }
+  return "Oi! Sheep number " + (queue.length - index - 1) + "! You are about to be eaten by a wolf!";
+}
